892ee42c27b8a8e5d964578fe102b87f102bfa39
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-10 19:03:22 -0600

    Make website

---
+++ js/Lorem.js
@@ -0,0 +1,17 @@
+// Lorem.js
+// By @Wish13yt on GitHub
+// Please give credit!
+function generateLorem() {
+    let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis ultricies velit eu dictum. Donec et iaculis nisi. Nullam urna tortor, volutpat quis neque a, feugiat gravida nibh. Aliquam condimentum nec purus vitae iaculis. Cras pellentesque feugiat metus quis hendrerit. Vivamus viverra est tempus faucibus vulputate. Donec quis congue purus. Maecenas eget nisl leo. Suspendisse potenti. Suspendisse lobortis augue at congue porta."
+    text.innerText = lorem
+    // thanks https://stackoverflow.com/questions/2803880/is-there-a-way-to-get-a-textarea-to-stretch-to-fit-its-content-without-using-php
+    text.style.height = text.scrollHeight + "px"
+}
+function generateFreaky() {
+    // i apologize for any pain this may cause
+    // thanks https://blox-fruits.fandom.com/f/p/4400000000000413207 for the word list
+    let brainrot = "Skibidi down in Ohio gyat kai cenat baby gronk Livvy dune ice spice rizzing rizzler rizzed of oz rizzasaurus rex Minecraft mobs in real life sigma grindset alpha beta male lone wolf  do you have skibidi rizz?, grimace shake, level six gyat fanum taxing ai generated meme compilation subway surfers family guy clips erm what the sigma  "
+    text.innerText = brainrot
+    // thanks https://stackoverflow.com/questions/2803880/is-there-a-way-to-get-a-textarea-to-stretch-to-fit-its-content-without-using-php
+    text.style.height = text.scrollHeight + "px"
+}
